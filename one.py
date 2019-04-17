da1e2924f81e842fe1597e8030dfb1513dde74a1
ROHIT RAWAT <rohit@spoton.com>
2019-04-17 16:40:24 -0500

    naive solution problem one

---
+++ one.py
@@ -0,0 +1,9 @@
+
+
+def verify_sum(arr, k):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] + arr[j] == k:
+                return True
+
+    return False
---
+++ test_1.py
@@ -0,0 +1,24 @@
+"""
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+Bonus: Can you do this in one pass?
+"""
+import pytest
+
+from one import verify_sum
+
+
+class TestSum:
+
+    def test_only_two_numbers(self):
+        assert verify_sum([1, 2], 3) == True
+        assert verify_sum([3, 4], 5) == False
+    
+    @pytest.mark.parametrize("arr,k,expected",
+        [([1, 1, 1],2,True),
+         ([2, 3, 4], 6, True),
+         ([2, 3, 4], 8, False)
+        ],
+    )
+    def test_three_numbers(self, arr, k, expected):
+        assert verify_sum(arr, k) == expected
