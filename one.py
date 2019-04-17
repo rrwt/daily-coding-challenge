f09008cfcee976a90438fe40a245ab728565733c
ROHIT RAWAT <rohit@spoton.com>
2019-04-17 17:16:59 -0500

    Problem 1. added efficient solution

--- one.py
+++ one.py
@@ -1,9 +1,41 @@
-
 # naive solution O(n*n)
-def verify_sum(arr, k):
-    for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
-            if arr[i] + arr[j] == k:
+def verify_sum(ar, value):
+    for i in range(len(ar)):
+        for j in range(i+1, len(ar)):
+            if ar[i] + ar[j] == value:
                 return True
 
     return False
+
+
+# more efficient solution O(n*logn)
+def verify_sum_2(ar, value):
+    ar.sort()
+    i = 0
+    j = len(ar) - 1
+
+    while i < j:
+        sum_ = ar[i] + ar[j]
+
+        if sum_ == value:
+            return True
+        elif sum_ > value:
+            j = j-1
+        else:
+            i = i+1
+
+    return False
+
+
+if __name__ == "__main__":
+    import random
+
+    for _ in range(1000_000):
+        arr = random.sample(range(1, 1000_000), 10)
+        k = random.randint(100, 1000_000)
+
+        if verify_sum(arr, k) != verify_sum_2(arr, k):
+            """a test to verify that everything is working fine in the efficient solution"""
+            print(arr)
+            print(k)
+            break
--- test_1.py
+++ test_1.py
@@ -5,11 +5,10 @@ Bonus: Can you do this in one pass?
 """
 import pytest
 
-from one import verify_sum
+from one import verify_sum, verify_sum_2
 
 
 class TestSum:
-
     def test_only_two_numbers(self):
         assert verify_sum([1, 2], 3) == True
         assert verify_sum([3, 4], 5) == False
@@ -22,3 +21,18 @@ class TestSum:
     )
     def test_three_numbers(self, arr, k, expected):
         assert verify_sum(arr, k) == expected
+
+
+class TestSum2:
+    def test_only_two_numbers(self):
+        assert verify_sum_2([1, 2], 3) == True
+        assert verify_sum_2([3, 4], 5) == False
+    
+    @pytest.mark.parametrize("arr,k,expected",
+        [([1, 1, 1],2,True),
+         ([2, 3, 4], 6, True),
+         ([2, 3, 4], 8, False)
+        ],
+    )
+    def test_three_numbers(self, arr, k, expected):
+        assert verify_sum_2(arr, k) == expected
