d183f3614978fbc80462892dd3c8157b7ea6a4f9
ROHIT RAWAT <rohit@spoton.com>
2019-04-18 23:42:57 -0500

    problem 5: using closure cell content

---
+++ problems/problem_5.py
@@ -0,0 +1,20 @@
+"""
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of
+that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Implement car and cdr.
+"""
+
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+
+def car(func):
+    return func.__closure__[0].cell_contents
+
+
+def cdr(func):
+    return func.__closure__[1].cell_contents
