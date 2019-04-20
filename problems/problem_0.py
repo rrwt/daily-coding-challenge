2e093a82a2054d6177b0d4d1b26a0c44b71a7aa8
ROHIT RAWAT <rohit@spoton.com>
2019-04-19 22:06:35 -0500

    added heapsort method to resolve the issue. TODO: Create and sort binary heap without using inbuilt functions

--- problems/problem_0.py
+++ problems/problem_0.py
@@ -1,11 +1,14 @@
+import heapq
+
+
 """
 return a new sorted merged list from K sorted lists, each with size N
 """
 
 
 # naive solution: O(knlog(kn))
-def merge_sorted_lists(arrays, k):
-    """Merge k arrays 
+def merge_sorted_lists(arrays):
+    """Merge k arrays
 
     Args:
         arrays (list): A list of list
@@ -20,3 +23,21 @@ def merge_sorted_lists(arrays, k):
         res.extend(arr)
 
     return sorted(res)
+
+
+def merge_sorted_lists_min_heap(arrays):
+    """An approach to efficiently merge k arrays using min heap.
+    This approach takes advantage of the fact that all the arrays
+    are sorted.
+
+    Time complexity: Kn
+    Time taken to create and merge a min heap is proportional to number of elements
+
+    Args:
+        arrays (list): A list of list
+        k (integer): Number of lists
+
+    Returns:
+        list: flattened and sorted list
+    """
+    return list(heapq.merge(*arrays))
--- problems/test_0.py
+++ problems/test_0.py
@@ -3,19 +3,34 @@ return a new sorted merged list from K sorted lists, each with size N
 """
 import pytest
 
-from .problem_0 import merge_sorted_lists
+from .problem_0 import merge_sorted_lists, merge_sorted_lists_min_heap
 
 
 class TestMergeKSortedList:
     def test_given_two_lists_with_same_elements_each_returns_a_list_of_two_elements(self):
-        assert merge_sorted_lists([[1], [1]], 2) == [1, 1]
+        assert merge_sorted_lists([[1], [1]]) == [1, 1]
 
     def test_given_two_lists_with_two_different_elements_returns_a_sorted_list(self):
-        assert merge_sorted_lists([[1], [2]], 2) == [1, 2]
+        assert merge_sorted_lists([[1], [2]]) == [1, 2]
 
-    @pytest.mark.parametrize("input_lists,k,expected_output", [
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3, [1, 2, 3, 4, 5, 6, 7, 8, 9]),
-        ([[20, 25, 30], [10, 15, 22]], 2, [10, 15, 20, 22, 25, 30])
+    @pytest.mark.parametrize("input_lists,expected_output", [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        ([[20, 25, 30], [10, 15, 22]], [10, 15, 20, 22, 25, 30])
     ])
-    def test_given_k_lists_returns_a_sorted_list(self, input_lists, k, expected_output):
-        assert merge_sorted_lists(input_lists, k) == expected_output
+    def test_given_k_lists_returns_a_sorted_list(self, input_lists, expected_output):
+        assert merge_sorted_lists(input_lists) == expected_output
+
+
+class TestMergeKSortedListUsingMinHeap:
+    def test_given_two_lists_with_same_elements_each_returns_a_list_of_two_elements(self):
+        assert merge_sorted_lists_min_heap([[1], [1]]) == [1, 1]
+
+    def test_given_two_lists_with_two_different_elements_returns_a_sorted_list(self):
+        assert merge_sorted_lists_min_heap([[1], [2]]) == [1, 2]
+
+    @pytest.mark.parametrize("input_lists,expected_output", [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        ([[20, 25, 30], [10, 15, 22]], [10, 15, 20, 22, 25, 30])
+    ])
+    def test_given_k_lists_returns_a_sorted_list(self, input_lists, expected_output):
+        assert merge_sorted_lists_min_heap(input_lists) == expected_output
