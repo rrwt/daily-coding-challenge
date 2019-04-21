ae5bd86cd99d8307211dcdf009ea979f4bb42000
ROHIT RAWAT <rohit@spoton.com>
2019-04-20 20:50:23 -0500

    solved using include and exclude sums

--- problems/problem_9.py
+++ problems/problem_9.py
@@ -4,11 +4,27 @@ Numbers can be 0 or negative.
 
 For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5]
 should return 10, since we pick 5 and 5.
+
+Follow-up: Can you do this in O(N) time and constant space?
 """
 
 
+# O(n) and constant space
 def largest_non_adjacent_sum(input_list):
     if not input_list:
         return None
 
-    return max(input_list)
+    length = len(input_list)
+
+    if length == 1:
+        return input_list[0]
+
+    max_sum_inc_cur = input_list[0]
+    max_sum_exc_cur = 0
+
+    for elem in input_list[1:]:
+        temp = max(max_sum_exc_cur, max_sum_exc_cur + elem)
+        max_sum_exc_cur = max(max_sum_inc_cur, max_sum_exc_cur)
+        max_sum_inc_cur = temp
+
+    return max(max_sum_inc_cur, max_sum_exc_cur)
--- problems/test_9.py
+++ problems/test_9.py
@@ -4,6 +4,8 @@ Numbers can be 0 or negative.
 
 For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5]
 should return 10, since we pick 5 and 5.
+
+Follow-up: Can you do this in O(N) time and constant space?
 """
 import pytest
 
@@ -25,3 +27,18 @@ class TestLargestSum:
     ])
     def test_list_with_two_integers_should_return_the_higher_value(self, given_input, expected):
         assert largest_non_adjacent_sum(given_input) == expected
+
+    @pytest.mark.parametrize("given_input,expected", [
+        ([1, 2, 3], 4), ([3, 5, 1], 5), ([-10, 15, 20], 20)
+    ])
+    def test_list_of_3_ints_should_return_max_non_adjacent_sum(self, given_input, expected):
+        assert largest_non_adjacent_sum(given_input) == expected
+
+    @pytest.mark.parametrize("given_input,expected", [
+        ([1, 2, 3, 4], 6), ([3, 1, 2, 4], 7), ([3, 4, -1, -3], 4)
+    ])
+    def test_list_of_4_ints_should_return_max_non_adjacent_sum(self, given_input, expected):
+        assert largest_non_adjacent_sum(given_input) == expected
+
+    def test_sample_problem(self):
+        assert largest_non_adjacent_sum([2, 4, 6, 2, 5]) == 13
