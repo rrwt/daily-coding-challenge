0656def56d8514d9d234372bfc18cb0de262b317
ROHIT RAWAT <rohit@spoton.com>
2019-04-28 18:55:10 -0500

    added solution for palindrome permutation

--- ctci/ch1/palindrome_permutation.py
+++ ctci/ch1/palindrome_permutation.py
@@ -4,3 +4,24 @@ palin­ drome. A palindrome is a word or phrase that is the same forwards
 and backwards. A permutation is a rearrangement of letters.
 The palindrome does not need to be limited to just dictionary words.
 """
+import collections
+
+
+def is_permutation_palindrome(str_in):
+    str_in = ''.join(str_in.strip().split())
+    d = collections.defaultdict(int)
+    l = len(str_in)
+
+    tolerance = l % 2
+
+    for s in str_in:
+        d[s.lower()] += 1
+
+    for v in d.values():
+        if v % 2 == 1 and not tolerance:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    print(is_permutation_palindrome("Tact Coa"))
