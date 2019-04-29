a4b2508ca754dc915c2c56c4d0bba70569bcbd83
ROHIT RAWAT <rohit@spoton.com>
2019-04-28 21:27:43 -0500

    string compression

---
+++ ctci/ch1/string_compression.py
@@ -0,0 +1,34 @@
+"""
+Implement a method to perform basic string compression using the counts of
+repeated characters. For example, the string aabcccccaaa would become
+a2b1c5a3. If the "compressed" string would not become smaller than the
+original string, your method should return the original string.
+You can assume the string has only uppercase and lowercase letters (a - z).
+"""
+
+
+# O(n)
+def compress(str_in):
+    l = len(str_in)
+    i = 0
+    res = ""
+
+    while i < l:
+        count = 1
+        cur = str_in[i]
+        i += 1
+
+        while i < l:
+            if str_in[i] != cur:
+                break
+            count += 1
+            i += 1
+
+        res = f'{res}{cur}{count}'
+
+    return res if len(res) < l else str_in
+
+
+if __name__ == '__main__':
+    assert compress('aabcccccaaa') == 'a2b1c5a3'
+    test()
