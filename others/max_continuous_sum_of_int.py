ec32a10f21311f7632f74cdb34f4a611d110a98c
ROHIT RAWAT <rohit@spoton.com>
2019-05-10 13:05:28 -0500

    Solution to max continuous sum of integers

---
+++ others/max_continuous_sum_of_int.py
@@ -0,0 +1,22 @@
+"""
+Find the maximum sum of continuous subarray in a given array of integers
+containing +ve and -ve integers
+"""
+
+
+def continuous_sum(arr):
+    max_ = curr_sum = arr[0]
+    l = len(arr)
+
+    for i in range(1, l):
+        new_sum = max(curr_sum + arr[i], 0)
+        curr_sum = max(new_sum, arr[i])
+
+        if curr_sum > max_:
+            max_ = curr_sum
+
+    return max(max_, 0)
+
+
+if __name__ == '__main__':
+    print(continuous_sum([-2, 11, -4, 13, -5, -2]))
