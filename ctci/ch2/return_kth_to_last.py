f0156cac855b1e3ee01b02d06d5aca0780fb0f9f
ROHIT RAWAT <rohit@spoton.com>
2019-05-13 14:49:52 -0500

    find kth element from last

---
+++ ctci/ch2/return_kth_to_last.py
@@ -0,0 +1,33 @@
+"""
+Implement an algorithm to find the kth to last element of a singly linked list.
+"""
+
+
+def kth_to_last_element(llist: list, k: int) -> int:
+    """
+    O(n) & O(1)
+    Steps:
+        1. find length of linked list.
+        2. move first pointer k steps ahead
+        3. move second and first pointer until first pointer becomes none
+        4. return the data at second pointer
+    """
+    l: int = len(llist)
+
+    if l < k:
+        return None
+
+    i: int = k
+    j: int = 0
+
+    while i < l:
+        i += 1
+        j += 1
+
+    return llist[j]
+
+
+if __name__ == "__main__":
+    llist: list = [7, 6, 5, 4, 3, 2, 1]
+    print(llist)
+    assert kth_to_last_element(llist, 3) == 3
