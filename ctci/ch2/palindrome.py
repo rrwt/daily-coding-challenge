c68f1ab36271f6458f552fa3b82e41c15f453571
ROHIT RAWAT <rohit@spoton.com>
2019-05-14 16:08:36 -0500

    solution palindrome list.

---
+++ ctci/ch2/palindrome.py
@@ -0,0 +1,35 @@
+"""
+Implement a function to check if a linked list is a palindrome.
+"""
+from list_structure import Node, LinkedList
+
+
+def palindrome(ll: LinkedList) -> bool:
+    """
+    We can recursively call the list and it's reverse and check if they match
+    """
+    def palindrome_recursive(t: Node) -> bool:
+        nonlocal head
+
+        if not t:
+            return True
+
+        p: bool = palindrome_recursive(t.next)
+
+        if p and head.data == t.data:
+            head = head.next
+            return True
+
+        return False
+
+    head: Node = ll.head
+    tail: Node = ll.head
+
+    return palindrome_recursive(tail)
+
+
+if __name__ == "__main__":
+    ll = LinkedList(Node(1))
+    ll.push(2).push(3).push(4).push(3).push(2).push(1)
+
+    assert palindrome(ll) == True
