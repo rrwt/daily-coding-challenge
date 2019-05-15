036c5452890f16d6f16c35c496bc7778b2b3c77d
ROHIT RAWAT <rohit@spoton.com>
2019-05-15 12:45:29 -0500

    more solution to intersection problem + loop detection

--- ctci/ch2/intersection.py
+++ ctci/ch2/intersection.py
@@ -5,6 +5,8 @@ on reference, not value. That is, if the kth node of the first linked list
 is the exact same node (by reference) as the jth node of the second linked
 list, then they are intersecting.
 """
+from typing import Optional
+
 from list_structure import LinkedList, Node
 
 
@@ -29,10 +31,64 @@ def intersect(ll1: LinkedList, ll2: LinkedList) -> bool:
 
     while h:
         if h in s:
-            return True
+            return h  # return the intersecting node
         h = h.next
 
     return False
+
+
+def get_loop(ll: LinkedList) -> (bool, Optional[Node]):
+    """
+    Return the loop node and if there is a loop. Floyd's algorithm
+    Note: Cannot find the length by counting the number of nodes untill None
+    """
+    slow: Node = ll.head
+    fast: Node = ll.head
+
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+        if fast == slow:
+            break
+
+    if not fast:
+        return False, None
+
+    """
+    If we reinitialize the slow pointer and both pointers move at the
+    same pace(slow), the intersection is found at the point they meet.
+    """
+    slow = ll.head
+
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+
+    return True, slow
+
+
+def intersect_without_extra_space(ll1: LinkedList, ll2: LinkedList) -> bool:
+    """
+    Assumption, end parts of the joined linkedlists is same (singly linked list).
+    Solution: Make first list circular. Detect circle in the second list.
+    If there is a circle, it means they intersect; otherwise no.
+    Remove the circle from the first list.
+    time complexity: O(n+m)
+    extra space complexity: O(1)
+    """
+    h1: Node = ll1.head
+
+    while h1.next:
+        h1 = h1.next
+
+    h1.next = ll1.head
+
+    has_loop, node = get_loop(ll2)
+
+    h1.next = None
+
+    return node or has_loop
 
 
 if __name__ == "__main__":
@@ -46,10 +102,17 @@ if __name__ == "__main__":
     ll2.head.next = node
     ll1.head.next.next.next = node
 
-    assert intersect(ll1, ll2) == True
+    node = intersect(ll1, ll2)
+    print('intersecting node is', node.data, sep=' ')
 
     ll3: LinkedList = LinkedList(Node(11))
     ll3.push(13).push(15).push(17)
 
     assert intersect(ll1, ll3) == False
     assert intersect(ll2, ll3) == False
+
+    node = intersect_without_extra_space(ll1, ll2)
+    print('interseting node is', node.data, sep=' ')
+
+    assert intersect_without_extra_space(ll1, ll3) == False
+    assert intersect_without_extra_space(ll2, ll3) == False
