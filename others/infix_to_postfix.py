c08c1e3ff4b3325ba5446caebb6f358ab85fc15d
ROHIT RAWAT <rohit@spoton.com>
2019-05-19 11:53:07 -0500

    added postfix to infix solution

--- others/infix_to_postfix.py
+++ others/infix_to_postfix.py
@@ -15,6 +15,16 @@ def precedence(operator: str) -> int:
 
 
 def infix_to_postfix(infix: str) -> str:
+    """
+    read from left to right
+    1. operand -> store in output
+    2. for operator
+        i. if empty stack or operand precedence > stack's top operand precedence, then push
+        ii. otherwise pop and append to result until previous condition is met
+    3. for (: push to stack
+    4. for ): pop and add to output until ( occurs
+    5. repeat
+    """
     result: str = ""
     stack: list = []
 
@@ -41,7 +51,28 @@ def infix_to_postfix(infix: str) -> str:
     return result
 
 
+def postfix_to_infix(postfix: str) -> str:
+    """
+    start reading from beginning
+    1. every operand gets pushed into the stack
+    2. for every operator, pop 2 operands and push (op1 op op2) back into stack
+    3. repeat
+    """
+    stack: list = []
+
+    for c in postfix:
+        if c.isalpha():
+            stack.append(c)
+        else:
+            op2: str = stack.pop()
+            op1: str = stack.pop()
+            stack.append(f"({op1}{c}{op2})")
+
+    return stack[0]
+
+
 if __name__ == "__main__":
     assert infix_to_postfix("a+b*c+d") == "abc*+d+"
     assert infix_to_postfix("A*(B+C*D)+E") == "ABCD*+*E+"
     assert infix_to_postfix("(A+B)*(C-D)") == "AB+CD-*"
+    assert postfix_to_infix("abc*+d+") == "((a+(b*c))+d)"
