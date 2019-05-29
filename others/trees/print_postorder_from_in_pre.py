224c620735536370d5ed80b40577b6e13a815fe8
ROHIT RAWAT <rohit@spoton.com>
2019-05-29 12:26:32 -0500

    optimized solution for postorder traversal from inorder and preorder traversal

--- others/trees/print_postorder_from_in_pre.py
+++ others/trees/print_postorder_from_in_pre.py
@@ -29,6 +29,37 @@ def print_postorder(inorder: list, preorder: list, length: int):
     print(root, end=" ")
 
 
+def print_postorder_optimised(inorder: list, preorder: list):
+    """
+    Using extra O(n) space, this could be optimized for searchin the root index in inorder array
+    time: O(n)
+    space: O(n)
+    """
+
+    def postorder(start: int, end: int):
+        nonlocal pre_index
+
+        if start > end:
+            return None
+
+        root = preorder[pre_index]
+        pre_index += 1
+        root_index = hashmap[root]
+
+        if root_index > start:  # left
+            postorder(start, root_index - 1)
+        if root_index < end:  # right
+            postorder(root_index + 1, end)
+
+        print(root, end=" ")
+
+    hashmap = {v: k for k, v in enumerate(inorder)}
+    pre_index = 0
+    postorder(0, len(inorder) - 1)
+
+
 if __name__ == "__main__":
     print_postorder([4, 2, 5, 1, 3, 6], [1, 2, 4, 5, 3, 6], 6)
+    print()
+    print_postorder_optimised([4, 2, 5, 1, 3, 6], [1, 2, 4, 5, 3, 6])
     print()
