fe22d0f2ad5a56b7a8ba42881809e6df6c14b758
ROHIT RAWAT <rohit@spoton.com>
2019-06-03 13:28:14 -0500

    tree density

---
+++ others/trees/tree_density.py
@@ -0,0 +1,35 @@
+"""
+Get density of a binary tree in one traversal
+"""
+from binary_tree_node import Node  # type: ignore
+
+
+def density(root: Node) -> float:
+    def inorder(root: Node, height: int = 1):
+        nonlocal nodes
+        nonlocal max_height
+        if root:
+            inorder(root.left, height + 1)
+            max_height = max(height, max_height)
+            nodes += 1
+            inorder(root.right, height + 1)
+
+    nodes: int = 0
+    max_height: int = 0
+
+    inorder(root)
+    print("nodes:", nodes, "; max_height:", max_height)
+
+    return nodes / max_height if max_height else 0
+
+
+if __name__ == "__main__":
+
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.right = Node(4)
+    root.right.right = Node(5)
+
+    print(density(root))
+
