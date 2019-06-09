3468671dd20bb323cf42a1b5e9efb4d3be52d8b7
ROHIT RAWAT <rohit@spoton.com>
2019-06-09 13:06:20 -0500

    if a binary tree is bst using inorder traversal

--- gfg/bst/check_if_bin_tree_is_bst.py
+++ gfg/bst/check_if_bin_tree_is_bst.py
@@ -10,6 +10,12 @@ from bst_and_node import Node  # type: ignore
 
 
 def is_bst(root: Node) -> bool:
+    """
+    time complexity: O(n)
+    Space complexity: O(n)
+    :param root: Node
+    :return: bool
+    """
     def check_bst(node: Optional[Node], min_: int, max_: int) -> bool:
         if not node:
             return True
@@ -22,6 +28,30 @@ def is_bst(root: Node) -> bool:
     return check_bst(root, -1000_000, 1000_000)
 
 
+def is_bst_using_in_order_traversal(root: Node) -> bool:
+    """
+    time complexity: O(n)
+    Space complexity: O(n)
+    :param root: Node
+    :return: bool
+    """
+    def in_order(node: Optional[Node]):
+        nonlocal elems
+        if node:
+            in_order(node.left)
+            elems.append(node.data)  # should be sorted
+            in_order(node.right)
+
+    elems: list = []
+    in_order(root)
+
+    for i in range(1, len(elems)):
+        if elems[i-1] > elems[i]:
+            return False
+
+    return True
+
+
 if __name__ == "__main__":
     root = Node(4)
     root.left = Node(2)
@@ -29,3 +59,4 @@ if __name__ == "__main__":
     root.left.left = Node(1)
     root.left.right = Node(3)
     print(is_bst(root))
+    print(is_bst_using_in_order_traversal(root))
