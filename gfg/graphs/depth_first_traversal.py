6a29ad59238564ecf8512af8a5562f4d18c9a7f8
ROHIT RAWAT <rohit@spoton.com>
2019-06-22 16:28:23 -0500

    depth first traversal of a graph

---
+++ gfg/graphs/depth_first_traversal.py
@@ -0,0 +1,49 @@
+"""
+Depth first search treaversal of an undirected graph
+"""
+import math
+
+from ds import GraphM  # type: ignore
+
+
+def depth_first_traversal(g: list, first_node: int) -> None:
+    """
+    Time complexity: O(V)
+    Space complexity: O(V)
+    """
+    if g:
+        stack: list = []
+        visited: set = set()
+        stack.append(first_node)
+        visited.add(first_node)
+
+        while stack:
+            node = stack.pop()
+            print(node, end=" ")
+
+            for element, connection in enumerate(g[node]):
+                if connection and element not in visited and connection < math.inf:
+                    stack.append(element)
+                    visited.add(element)
+
+
+if __name__ == "__main__":
+    g = GraphM(4)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(1, 2)
+    g.add_edge(2, 0)
+    g.add_edge(2, 3)
+    g.add_edge(3, 3)
+    print("dfs starting at node 2")
+    depth_first_traversal(g.graph, 2)
+    print()
+    print("dfs starting at node 1")
+    depth_first_traversal(g.graph, 1)
+    print()
+    print("dfs starting at node 0")
+    depth_first_traversal(g.graph, 0)
+    print()
+    print("dfs starting at node 3")
+    depth_first_traversal(g.graph, 3)
+    print()
