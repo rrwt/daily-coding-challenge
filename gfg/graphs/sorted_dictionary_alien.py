0f1566a29f3435ecf094a9ac3ba2d150a0bec585
ROHIT RAWAT <rohit@spoton.com>
2019-07-06 08:47:37 -0500

    sorted alien dictionary

---
+++ gfg/graphs/sorted_dictionary_alien.py
@@ -0,0 +1,29 @@
+"""
+Given a sorted dictionary (array of words) of an alien language,
+find order of characters in the language.
+"""
+from ds import GraphM  # type: ignore
+from topological_sort import topological_sort_using_dfs  # type: ignore
+
+
+def order_of_characters(words: tuple, num_char: int) -> list:
+    """
+    Order can be found using a DAG.
+    The dictionary is sorted, therefore we can compare characters of each word to form DAG.
+    Afterwords topological sorting can be applied to find the final order
+    """
+    l = len(words)
+    graph = GraphM(num_char, "directed")
+
+    for i in range(l - 1):
+        for c1, c2 in zip(words[i], words[i + 1]):
+            if c1 != c2:
+                graph.add_edge(ord(c1) - 97, ord(c2) - 97)
+                break
+
+    return topological_sort_using_dfs(graph.graph, num_char)
+
+
+if __name__ == "__main__":
+    words = "baa", "abcd", "abca", "cab", "cad"
+    print(list(map(lambda x: chr(x + 97), order_of_characters(words, 4))))
