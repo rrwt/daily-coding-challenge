f17802e58f1b03a51347593b659417189aa374d0
ROHIT RAWAT <rohit@spoton.com>
2019-07-09 15:13:49 -0500

    divide and conquer

---
+++ algorithms/divide_and_conquer/power_of_number.py
@@ -0,0 +1,23 @@
+"""
+Given two integers x and n, write a function to compute x^n.
+We may assume that x and n are small and overflow doesn’t happen.
+"""
+
+
+def power(x: int, n: int) -> int:
+    if n == 0:
+        return 1
+    if n == 1:
+        return x
+    if n == -1:
+        return 1 / x
+    temp = power(x, n // 2)
+    if n % 2 == 0:
+        return temp * temp
+    else:
+        return x * temp * temp if n > 0 else temp * temp / x
+
+
+if __name__ == "__main__":
+    print(power(5, -2))
+    print(power(5, 2))
