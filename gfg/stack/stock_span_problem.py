3d21a724b96b611c74eac6124f0225c2ef4adc5a
ROHIT RAWAT <rohit@spoton.com>
2019-09-02 10:30:01 -0500

    stock span problem - efficient solution

--- gfg/stack/stock_span_problem.py
+++ gfg/stack/stock_span_problem.py
@@ -12,9 +12,7 @@ from array import array
 
 def stock_span(prices: array) -> list:
     """
-    for each array element we can pop stack elements until the stack is not empty
-    or the top of stack is greater than the current element.
-    the count of popped elements + 1 is the span of that day.
+    Time Complexity: O(n*n)
     """
     span_values: list = []
 
@@ -31,5 +29,30 @@ def stock_span(prices: array) -> list:
     return span_values
 
 
+def stock_span_efficient(prices: array) -> list:
+    """
+    Store index of largest elements so far in stack
+    for each array element we can pop stack elements until the stack is not empty
+    or the top of stack is greater than the current element.
+    the count of popped elements + 1 is the span of that day.
+    Time Complexity: O(n)
+    """
+    stack: list = []
+    result: list = []
+
+    for index, value in enumerate(prices):
+        while stack and prices[stack[-1]] < value:
+            stack.pop()
+        if stack:
+            result.append(index - stack[-1])
+        else:
+            result.append(index + 1)
+
+        stack.append(index)
+
+    return result
+
+
 if __name__ == "__main__":
     print(stock_span(array("B", [100, 80, 60, 70, 60, 75, 85])))
+    print(stock_span_efficient(array("B", [100, 80, 60, 70, 60, 75, 85])))
