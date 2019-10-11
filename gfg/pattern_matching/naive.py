830ccbd84bfa0a1d56b0081f3a55e95907ad8013
ROHIT RAWAT <rohit@spoton.com>
2019-10-11 10:51:20 -0500

    naive pattern matching

---
+++ gfg/pattern_matching/naive.py
@@ -0,0 +1,35 @@
+"""Brute Force Pattern Matching
+"""
+
+
+def find_substring(string: str, substring: str) -> int:
+    """
+    Find substring in a larger string.
+    """
+    i_str: int = 0
+    l_str: int = len(string)
+    l_sub: int = len(substring)
+
+    while i_str <= l_str - l_sub:
+        index = 0
+
+        while index < l_sub and string[index + i_str] == substring[index]:
+            index += 1
+
+        if index >= l_sub:
+            return i_str
+        elif index + i_str >= l_str:
+            return -1
+
+        i_str += 1
+
+    return -1
+
+
+if __name__ == "__main__":
+    index = find_substring("THIS IS A TEST TEXT", "TEXT")
+
+    if index > -1:
+        print(f"Found at index {index}")
+    else:
+        print("Not Found")
