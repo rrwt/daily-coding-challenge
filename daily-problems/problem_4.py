9f3e2332de4b1ed94bd613009a190a3acf4bb27a
Rohit Rawat <rohitrawat2000@gmail.com>
2020-07-11 20:07:12 -0500

    solution 4: without using a set

--- daily-problems/problem_4.py
+++ daily-problems/problem_4.py
@@ -5,6 +5,38 @@ The array can contain duplicates and negative numbers as well.
 """
 
 
+def first_missing_positive_integer_using_constant_space(arr):
+    length = len(arr)
+
+    # part 1: Segregate the array using +ve and-ve values
+    start, end = 0, length - 1
+
+    while True:
+        while start < length and arr[start] >= 0:
+            start += 1
+        while end >= 0 and arr[end] < 0:
+            end -= 1
+
+        if start >= end:
+            break
+
+        arr[start], arr[end] = arr[end], arr[start]
+
+    # part 2: change sign of values at index of existing +ve integers to -ve
+    for index in range(start):
+        value = abs(arr[index])
+
+        if value < start:
+            arr[value] = -abs(arr[value])
+
+    # part 3: find the missing value
+    for index in range(1, length):
+        if arr[index] > 0:
+            return index
+
+    return length
+
+
 # using set in O(n) and constant extra space
 def first_missing_positive_integer(arr):
     arr = set(arr)
--- daily-problems/test_4.py
+++ daily-problems/test_4.py
@@ -8,18 +8,22 @@ import pytest
 from .problem_4 import (
     first_missing_positive_integer,
     first_missing_positive_integer_using_extra_space,
+    first_missing_positive_integer_using_constant_space,
 )
 
 
 class TestFindFirstMissingPositiveInteger:
     def test_given_zero_should_return_one(self):
         assert first_missing_positive_integer([0]) == 1
+        assert first_missing_positive_integer_using_constant_space([0]) == 1
 
     def test_given_one_should_return_two(self):
         assert first_missing_positive_integer([1]) == 2
+        assert first_missing_positive_integer_using_constant_space([0]) == 1
 
     def test_given_two_should_return_one(self):
         assert first_missing_positive_integer([2]) == 1
+        assert first_missing_positive_integer_using_constant_space([0]) == 1
 
     @pytest.mark.parametrize(
         "input_arr,expected_output",
@@ -29,9 +33,10 @@ class TestFindFirstMissingPositiveInteger:
         self, input_arr, expected_output
     ):
         assert first_missing_positive_integer(input_arr) == expected_output
+        assert first_missing_positive_integer_using_constant_space([0]) == 1
 
 
-class TestFindFirstMissingPositiveIntegerWithOnExtraSpace:
+class TestFindFirstMissingPositiveIntegerWithExtraSpace:
     def test_given_zero_should_return_one(self):
         assert first_missing_positive_integer_using_extra_space([0]) == 1
 
