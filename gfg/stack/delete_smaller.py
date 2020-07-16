2981f250b01b96c4646b4b01675a3fc4810f1650
Rohit Rawat <rohitrawat2000@gmail.com>
2020-07-16 12:16:52 -0500

    refactored code

--- gfg/stack/delete_smaller.py
+++ gfg/stack/delete_smaller.py
@@ -5,19 +5,19 @@ e.g. input: [20, 10, 25, 30, 40], k = 2
 """
 from collections import deque
 
-from .stack import Stack  # type: ignore
+from gfg.stack.stack import Stack  # type: ignore
 
 
 def delete_smaller(elements: deque, k: int) -> deque:
     """
-    It's easier to use a stack to resoplve this problem because of problem's
-    nature of changing comparisions
+    It's easier to use a stack to resolve this problem because of problem's
+    nature of changing comparision
     """
     stack = Stack()
     stack.push(elements.popleft())
 
-    while elements and k:
-        while k and stack.head and stack.peek() < elements[0]:
+    while elements and k > 0:
+        while k > 0 and stack.head and stack.peek() < elements[0]:
             stack.pop()
             k -= 1
         stack.push(elements.popleft())
