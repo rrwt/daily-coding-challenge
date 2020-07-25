c1fbd2301b66a8d318cb677fed66cdf3bb9d78e5
Rohit Rawat <rohitrawat2000@gmail.com>
2020-07-24 19:58:30 -0500

    bugfix and added a new O(n) solution for problem 13

--- daily_problems/problem_13.py
+++ daily_problems/problem_13.py
@@ -4,7 +4,7 @@ contains at most k distinct characters.
 For example, given s = "abcba" and k = 2,
 the longest substring with k distinct characters is "bcb".
 """
-from collections import deque, defaultdict
+from collections import defaultdict
 
 
 def longest_substring(string: str, k: int) -> int:
@@ -14,27 +14,72 @@ def longest_substring(string: str, k: int) -> int:
     """
     length: int = len(string)
     max_count = min(length, k)
-    d: defaultdict = defaultdict(int)
-    q_l: int = 0
+    hash_map: defaultdict = defaultdict(int)
+    cur_char_len: int = 0
+    distinct_chars: int = 0
     starting_index: int = 0
 
     for index, char in enumerate(string):
-        if d[char] == 0 and q_l >= k:
+        if hash_map[char] == 0 and distinct_chars >= k:
             while starting_index < index:
+                # reduce count of all elements, while all elements have count > 0
                 el = string[starting_index]
-                d[el] -= 1
-                q_l -= 1
+                hash_map[el] -= 1
+                cur_char_len -= 1
                 starting_index += 1
-                if d[el] == 0:
+
+                if hash_map[el] == 0:
+                    distinct_chars -= 1
                     break
 
-        q_l += 1
-        d[char] += 1
-        max_count = max(max_count, q_l)
+        cur_char_len += 1
+
+        if hash_map[char] == 0:
+            distinct_chars += 1
+
+        hash_map[char] += 1
+        max_count = max(max_count, cur_char_len)
 
     return max_count
 
 
+def longest_substring_alt(string: str, k: int) -> int:
+    """
+    Time complexity: O(n)
+    Space Complexity: O(n)
+    """
+    hash_map = {}
+    distinct_chars = set()
+
+    for index, char in enumerate(string[:k]):
+        hash_map[char] = index
+        distinct_chars.add(char)
+
+    set_size = len(distinct_chars)
+    max_len = curr_len = k
+
+    for index, char in enumerate(string[k:], start=k):
+        if char not in distinct_chars:
+            if set_size == k:
+                char_to_remove = string[index-k]
+                distinct_chars.remove(char_to_remove)
+                curr_len = index - 1 - hash_map[char_to_remove]
+                set_size -= 1
+
+            distinct_chars.add(char)
+            set_size += 1
+
+        hash_map[char] = index
+        curr_len += 1
+        max_len = max(max_len, curr_len)
+
+    return max_len
+
+
 if __name__ == "__main__":
-    print(longest_substring("abcba", 2))
-    print(longest_substring("aabacbebebe", 3))
+    assert longest_substring("abcba", 2) == 3
+    assert longest_substring_alt("abcba", 2) == 3
+    assert longest_substring("aabacbebebe", 3) == 7
+    assert longest_substring_alt("aabacbebebe", 3) == 7
+    assert longest_substring("eceba", 3) == 4
+    assert longest_substring_alt("eceba", 3) == 4
