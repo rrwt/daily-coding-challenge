be9ab5fccf9afe85e210fde9e033bdb410f41001
Rohit Rawat <rohitrawat2000@gmail.com>
2020-07-27 10:32:34 -0500

    bugfix and code improvement

--- daily_problems/problem_28.py
+++ daily_problems/problem_28.py
@@ -21,39 +21,36 @@ you should return the following:
 from typing import List
 
 
-def get_spaced_string(string_list: List[str], string_len: int, k: int) -> str:
-    if string_len == k:
+def get_spaced_string(string_list: List[str], string_len: int, line_length: int) -> str:
+    if string_len == line_length:
         return " ".join(string_list)
 
     length_list: int = len(string_list)
 
     if length_list == 1:
-        return string_list[0].ljust(k, " ")
+        return string_list[0].ljust(line_length, " ")
 
-    spaces_required: int = k - string_len
+    spaces_required: int = line_length - string_len
     new_space_list: List[int] = [0] * length_list
     i: int = 0
 
     while spaces_required:
         new_space_list[i] += 1
-        i += 1
         spaces_required -= 1
+        i = (i+1) % (length_list - 1)  # the last word doesn't get extra space after it
 
-        if i == length_list - 1:
-            i = 0
-
-    for i in range(length_list):
+    for i in range(length_list - 1):  # the last word doesn't get extra space after it
         string_list[i] = string_list[i] + " " * new_space_list[i]
 
     return " ".join(string_list)
 
 
-def word_wrap(word_list: List[str], k: int) -> List[str]:
-    length: int = len(word_list)
-    length_arr: List[int] = [len(word) for word in word_list]
+def word_wrap(words: List[str], line_length: int) -> List[str]:
+    length: int = len(words)
+    length_arr: List[int] = [len(word) for word in words]
 
     if length <= 1:
-        return word_list
+        return words
 
     i: int = 0
     res: List[str] = []
@@ -65,15 +62,15 @@ def word_wrap(word_list: List[str], k: int) -> List[str]:
         while j < length:
             new_len = cur_len + length_arr[j] + 1  # 1 for extra space
 
-            if new_len > k:
+            if new_len > line_length:
                 break
             cur_len = new_len
             j += 1
 
-        if cur_len > k:
+        if cur_len > line_length:
             raise Exception("the algorithm was not implemented correctly")
 
-        res.append(get_spaced_string(word_list[i:j], cur_len, k))
+        res.append(get_spaced_string(words[i:j], cur_len, line_length))
         i = j
 
     return res
