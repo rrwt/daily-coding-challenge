cce4d52a18d341c327de71e6d5edb0996a86fb63
Rohit Rawat <rohitrawat2000@gmail.com>
2020-07-30 07:45:32 -0500

    problem 35: segregate rgb.  modified solution

--- daily_problems/problem_35.py
+++ daily_problems/problem_35.py
@@ -9,34 +9,32 @@ it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
 from array import array
 
 
-def segregate_rgb(arr: array) -> array:
+def segregate_rgb(input_arr: array) -> array:
     """
     Have 3 pointers, each tracking one code.
     Time Complexity: O(n)
     Space Complexity: O(1)
     """
-    r: int = 0
-    b: int = len(arr) - 1
-
-    while r < b and arr[r] == "R":
-        r += 1
-    while b > r and arr[b] == "B":
-        b -= 1
-    g = r
+    r, g, b = 0, 0, len(input_arr) - 1
 
     while g <= b:
-        if arr[g] == "R":
-            arr[g], arr[r] = arr[r], arr[g]
+        if input_arr[g] == "R":
+            input_arr[g], input_arr[r] = input_arr[r], input_arr[g]
             r += 1
-        elif arr[g] == "B":
-            arr[g], arr[b] = arr[b], arr[g]
+            g += 1
+        elif input_arr[g] == "B":
+            input_arr[g], input_arr[b] = input_arr[b], input_arr[g]
             b -= 1
         else:
             g += 1
 
-    return arr
+    return input_arr
 
 
 if __name__ == "__main__":
     arr: array = array("u", ["G", "B", "R", "R", "B", "R", "G", "R"])
     assert segregate_rgb(arr) == array("u", ["R", "R", "R", "R", "G", "G", "B", "B"])
+
+    arr: array = array("u", ["R", "G", "G", "R", "G", "B", "G", "B", "R", "R", "R", "G"])
+    assert segregate_rgb(arr) == array("u", ["R", "R", "R", "R", "R", "G", "G", "G", "G",
+                                       "G", "B", "B"])
