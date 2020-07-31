2be753adf2da75e32209ee452d9abe7bb0f3b02b
Rohit Rawat <rohitrawat2000@gmail.com>
2020-07-31 09:35:35 -0500

    problem 42: dp solution to find whether the sum is possible or not

--- daily_problems/problem_42.py
+++ daily_problems/problem_42.py
@@ -23,9 +23,7 @@ def subset_sum(
         for i in range(index, arr_size):
             if total + arr[i] <= k:
                 res.append(arr[i])
-                res, new_total = subset_sum(
-                    arr, k, arr_size, res, i + 1, total + arr[i]
-                )
+                res, new_total = subset_sum(arr, k, arr_size, res, i + 1, total + arr[i])
 
                 if new_total == k:
                     return res, new_total
@@ -35,5 +33,29 @@ def subset_sum(
     return res or None, total
 
 
+def subset_sum_dp(arr: list, k: int, arr_size: int) -> bool:
+    """
+    Time Complexity: O(sum * n)
+    Space Complexity: O(sum * n)
+    """
+    dp = [[False] * (k+1) for _ in range(arr_size+1)]
+
+    for _ in range(arr_size+1):
+        dp[_][0] = True
+
+    for ind in range(1, arr_size+1):
+        for cur_sum in range(1, k + 1):
+            if cur_sum >= arr[ind-1]:
+                # if current sum >= current element, then dp[ind][cur_sum] is true if
+                # dp[ind-1[cur_sum] is true - leave current element
+                # or dp[ind-1][cur_sum-arr[ind-1]] is true - take current element
+                dp[ind][cur_sum] = dp[ind-1][cur_sum] or dp[ind-1][cur_sum-arr[ind-1]]
+            else:
+                dp[ind][cur_sum] = dp[ind-1][cur_sum]
+
+    return dp[arr_size][k]
+
+
 if __name__ == "__main__":
     print(*subset_sum([12, 1, 61, 5, 9, 2], 24, 6, []), sep=", ")
+    print(subset_sum_dp([12, 1, 61, 5, 9, 2], 24, 6))
