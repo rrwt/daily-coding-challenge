b43ac185614b2f0dc0aea81520deacac6bf94821
Rohit Rawat <rohitrawat2000@gmail.com>
2020-08-01 18:14:05 -0500

    path greater than value

--- gfg/graphs/path_length_greater_than_k.py
+++ gfg/graphs/path_length_greater_than_k.py
@@ -1,14 +1,14 @@
 """
 Given a graph, a source vertex in the graph and a number k,
 find if there is a simple path (without any cycle) starting
-from given source and ending at any other vertex.
+from given source and ending at any other vertex that is greater than k.
 """
 import math
 
-from ds import GraphM  # type: ignore
+from gfg.graphs.ds import GraphM  # type: ignore
 
 
-def path(graph: list, source: int, k: int, vertices: int) -> list:
+def path_greater(graph: list, source: int, k: int, vertices: int) -> list:
     """
     Greedy solution won't always work. We'd need to try every possible
     path from source to all other vertices.
@@ -16,7 +16,7 @@ def path(graph: list, source: int, k: int, vertices: int) -> list:
     Time Complexity: O(n!)
     """
 
-    def backtrack(next_vertex: int, distance: int, visited: set, total: int) -> int:
+    def backtrack(next_vertex: int, dist: int, visited: set, total: int) -> int:
         nonlocal path
 
         if next_vertex in visited:
@@ -24,14 +24,14 @@ def path(graph: list, source: int, k: int, vertices: int) -> list:
 
         visited.add(next_vertex)
         path.append(next_vertex)
-        cur_total = total + distance
+        cur_total = total + dist
 
         if cur_total > k:
             return cur_total
 
-        for vertex, dist in enumerate(graph[next_vertex]):
-            if vertex not in visited and 0 < dist < math.inf:
-                cur_total = backtrack(vertex, dist, visited, cur_total)
+        for src, dist in enumerate(graph[next_vertex]):
+            if src not in visited and 0 < dist < math.inf:
+                cur_total = backtrack(src, dist, visited, cur_total)
 
                 if cur_total > k:
                     return cur_total
@@ -42,28 +42,28 @@ def path(graph: list, source: int, k: int, vertices: int) -> list:
 
     path: list = [source]
 
-    for vertex, distance in enumerate(graph[source]):
-        if 0 < distance < math.inf and backtrack(vertex, distance, {source}, 0) > k:
+    for source, distance in enumerate(graph[source]):
+        if 0 < distance < math.inf and backtrack(source, distance, {source}, 0) > k:
             return path
 
     return []
 
 
 if __name__ == "__main__":
-    graph = GraphM(9)
-    graph.add_edge(0, 1, 4)
-    graph.add_edge(0, 7, 8)
-    graph.add_edge(1, 2, 8)
-    graph.add_edge(1, 7, 11)
-    graph.add_edge(2, 3, 7)
-    graph.add_edge(2, 5, 4)
-    graph.add_edge(2, 8, 2)
-    graph.add_edge(3, 4, 9)
-    graph.add_edge(3, 5, 14)
-    graph.add_edge(4, 5, 10)
-    graph.add_edge(5, 6, 2)
-    graph.add_edge(6, 7, 1)
-    graph.add_edge(6, 8, 6)
-    graph.add_edge(7, 8, 7)
+    g = GraphM(9)
+    g.add_edge(0, 1, 4)
+    g.add_edge(0, 7, 8)
+    g.add_edge(1, 2, 8)
+    g.add_edge(1, 7, 11)
+    g.add_edge(2, 3, 7)
+    g.add_edge(2, 5, 4)
+    g.add_edge(2, 8, 2)
+    g.add_edge(3, 4, 9)
+    g.add_edge(3, 5, 14)
+    g.add_edge(4, 5, 10)
+    g.add_edge(5, 6, 2)
+    g.add_edge(6, 7, 1)
+    g.add_edge(6, 8, 6)
+    g.add_edge(7, 8, 7)
 
-    print(path(graph.graph, 0, 60, graph.num_vertices))
+    print(path_greater(g.graph, 0, 60, g.num_vertices))
