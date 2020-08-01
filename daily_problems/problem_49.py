290282f2f628b77da6ea839efd234905ab8baf09
Rohit Rawat <rohitrawat2000@gmail.com>
2020-08-01 06:13:32 -0500

    refactored to save array space

--- daily_problems/problem_49.py
+++ daily_problems/problem_49.py
@@ -8,22 +8,20 @@ since we would not take any elements.
 Do this in O(N) time.
 """
 import sys
-from copy import deepcopy
 from typing import Tuple
 
 
 def max_sum(arr: list) -> Tuple[list, int]:
     """
-    Inc array and value considers current maximum.
-    Exc array and value considers historical maximum.
+    Inc value considers current maximum.
+    Exc value considers historical maximum.
     Time Complexity: O(n)
     Space Complexity: O(1)
     Note: Extra time is taken to copy the inc array into exc array
     """
 
     sum_inc, sum_exc = -sys.maxsize, -sys.maxsize
-    arr_inc: list = []
-    arr_exc: list = []
+    start_inc, end_inc, start_exc, end_exc = -1, -1, -1, -1
     index = 0
 
     while index < len(arr):
@@ -31,24 +29,24 @@ def max_sum(arr: list) -> Tuple[list, int]:
 
         if el > el + sum_inc:
             sum_inc = el
-            arr_inc = [el]
+            start_inc, end_inc = index, index + 1
         else:
             sum_inc = el + sum_inc
-            arr_inc.append(el)
+            end_inc = index + 1
 
         if sum_inc > sum_exc:
             sum_exc = sum_inc
-            arr_exc = deepcopy(arr_inc)
+            start_exc, end_exc = start_inc, end_inc
         index += 1
 
     if sum_inc > 0 and sum_inc >= sum_exc:
-        return arr_inc, sum_inc
+        return arr[start_inc: end_inc], sum_inc
     elif sum_exc > 0 and sum_exc > sum_inc:
-        return arr_exc, sum_exc
+        return arr[start_exc: end_exc], sum_exc
     else:
         return [], 0
 
 
 if __name__ == "__main__":
-    for arr in [[34, -50, 42, 14, -5, 86], [-5, -1, -8, -9]]:
-        print("maximum sum subarray and sum for array", arr, "are", max_sum(arr))
+    for _ in [[34, -50, 42, 14, -5, 86], [-5, -1, -8, -9]]:
+        print("maximum sum subarray and sum for array", _, "are", max_sum(_))
