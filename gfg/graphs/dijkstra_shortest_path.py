3ed7a51335804a5fc0c5c0d840b814393c35c704
Rohit Rawat <rohitrawat2000@gmail.com>
2020-08-01 18:39:56 -0500

    refactored dijsktra's algorithm

--- gfg/graphs/dijkstra_shortest_path.py
+++ gfg/graphs/dijkstra_shortest_path.py
@@ -7,11 +7,12 @@ Given a graph and a source vertex, find the distance to all vertices from this v
 import sys
 from typing import Tuple
 
-from ds import GraphM  # type: ignore
+from gfg.graphs.ds import GraphM  # type: ignore
 
 
 def next_min(sp_set: set, distance: list) -> Tuple[int, int]:
     """
+    O(v)
     Get the next min distant element
     """
     min_dist = sys.maxsize
@@ -32,19 +33,18 @@ def dijsktra_sp(graph: list, source: int, vertices: int) -> list:
     Time complexity: O(V*V)
     """
     sp_set: set = set()
-    distance: list = [sys.maxsize for _ in range(vertices)]
+    distance: list = [sys.maxsize] * vertices
     distance[source] = 0
     parent: list = [None] * vertices  # only if path is important
 
     while len(sp_set) < vertices:
-        next_el, next_dist = next_min(sp_set, distance)
-        distance[next_el] = next_dist
-        sp_set.add(next_el)
+        cur_el, cur_dist = next_min(sp_set, distance)
+        sp_set.add(cur_el)
 
-        for vertex, dist in enumerate(graph[next_el]):
-            if vertex not in sp_set and distance[vertex] > dist + next_dist:
-                distance[vertex] = dist + next_dist
-                parent[vertex] = next_el
+        for dest, dist in enumerate(graph[cur_el]):
+            if dest not in sp_set and distance[dest] > dist + cur_dist:
+                distance[dest] = dist + cur_dist
+                parent[dest] = cur_el
 
     for i in range(vertices):
         if parent[i] is not None:
@@ -54,20 +54,20 @@ def dijsktra_sp(graph: list, source: int, vertices: int) -> list:
 
 
 if __name__ == "__main__":
-    graph = GraphM(9)
-    graph.add_edge(0, 1, 4)
-    graph.add_edge(0, 7, 8)
-    graph.add_edge(1, 2, 8)
-    graph.add_edge(1, 7, 11)
-    graph.add_edge(2, 3, 7)
-    graph.add_edge(2, 5, 4)
-    graph.add_edge(2, 8, 2)
-    graph.add_edge(3, 4, 9)
-    graph.add_edge(3, 5, 14)
-    graph.add_edge(4, 5, 10)
-    graph.add_edge(5, 6, 2)
-    graph.add_edge(6, 7, 1)
-    graph.add_edge(6, 8, 6)
-    graph.add_edge(7, 8, 7)
+    g = GraphM(9)
+    g.add_edge(0, 1, 4)
+    g.add_edge(0, 7, 8)
+    g.add_edge(1, 2, 8)
+    g.add_edge(1, 7, 11)
+    g.add_edge(2, 3, 7)
+    g.add_edge(2, 5, 4)
+    g.add_edge(2, 8, 2)
+    g.add_edge(3, 4, 9)
+    g.add_edge(3, 5, 14)
+    g.add_edge(4, 5, 10)
+    g.add_edge(5, 6, 2)
+    g.add_edge(6, 7, 1)
+    g.add_edge(6, 8, 6)
+    g.add_edge(7, 8, 7)
 
-    print(dijsktra_sp(graph.graph, 0, graph.num_vertices))
+    print(dijsktra_sp(g.graph, 0, g.num_vertices))
