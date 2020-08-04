43d0cda7f4e16459d5780594a63e7c0148640a53
Rohit Rawat <rohitrawat2000@gmail.com>
2020-08-04 18:17:57 -0500

    reverse ll inplace

--- daily_problems/problem_73.py
+++ daily_problems/problem_73.py
@@ -3,19 +3,19 @@ Given the head of a singly linked list, reverse it in-place.
 """
 from typing import Optional
 
-from linked_list import Node
+from .linked_list import Node
 
 
-def reverse_ll(head: Node) -> Node:
-    prev: Optional[Node] = None
+def reverse_ll(head_node: Node) -> Node:
+    prev_node: Optional[Node] = None
 
-    while head:
-        next = head.next
-        head.next = prev
-        prev = head
-        head = next
+    while head_node:
+        next_node = head_node.next
+        head_node.next = prev_node
+        prev_node = head_node
+        head_node = next_node
 
-    return prev
+    return prev_node
 
 
 if __name__ == "__main__":
