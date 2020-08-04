594a936d27145c1ded89c4cb7d6f14ccf1d88b00
Rohit Rawat <rohitrawat2000@gmail.com>
2020-08-04 06:52:07 -0500

    problem 65: print matrix in spiral form

--- daily_problems/problem_65.py
+++ daily_problems/problem_65.py
@@ -37,10 +37,9 @@ def print_matrix(matrix: list) -> None:
 
 
 if __name__ == "__main__":
-    matrix: list = [
+    print_matrix([
         [1, 2, 3, 4, 5],
         [6, 7, 8, 9, 10],
         [11, 12, 13, 14, 15],
         [16, 17, 18, 19, 20],
-    ]
-    print_matrix(matrix)
+    ])
