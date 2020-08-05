d50c237fe2778ed38af7fbc82c8da77b4e0cd49e
Rohit Rawat <rohitrawat2000@gmail.com>
2020-08-05 08:39:44 -0500

    refactored cutting a rod problem

--- gfg/dp/cutting_a_rod.py
+++ gfg/dp/cutting_a_rod.py
@@ -12,10 +12,7 @@ def cut_naive(length: int, prices: List[int]) -> int:
     if length == 1:
         return prices[0]
 
-    if length <= len(prices):
-        max_amount = prices[length - 1]
-    else:
-        max_amount = 0
+    max_amount = prices[length - 1] if length <= len(prices) else 0
 
     for index in range(length):
         max_amount = max(max_amount, prices[index] + cut_naive(length - index - 1, prices))
@@ -28,12 +25,10 @@ def cut_tabulated(prices: List[int]) -> int:
     values: List[int] = [0] * (rod_length + 1)
 
     for cur_length in range(1, rod_length + 1):
-        max_val = 0
+        values[cur_length] = prices[cur_length - 1]
 
-        for c in range(cur_length):
-            max_val = max(max_val, prices[c] + values[cur_length - c - 1])
-
-        values[cur_length] = max_val
+        for cut in range(cur_length // 2 + 1):
+            values[cur_length] = max(values[cur_length], values[cut] + values[cur_length - cut])
 
     return values[rod_length]
 
