747a26f3623bc8fb511b92e28e4fe42157fec2a3
Rohit Rawat <rohitrawat2000@gmail.com>
2020-08-08 20:07:13 -0500

    problem 91: anonymous functions in python

---
+++ daily_problems/problem_91.py
@@ -0,0 +1,28 @@
+"""
+What does the below code snippet print out?
+How can we fix the anonymous functions to behave as we'd expect?
+"""
+
+print("original implementation")
+functions = []
+for i in range(10):
+    functions.append(lambda: i)
+
+for f in functions:
+    print(f())
+
+
+# Solution
+# The lambda function is expected to print a variable i on its execution.
+# after the for loop finishes, the value of i becomes 9.
+# this i gets accessed by the lambda function when we try to execute it, i.e. in next for loop
+# therefore, when we invoke lambda using f(), we print 9 each time.
+# to fix it, we'd need to bind the value of i to anonymous function when we try to add it.
+
+print("new implementation")
+functions = []
+for i in range(10):
+    functions.append(lambda j=i: j)
+
+for f in functions:
+    print(f())
