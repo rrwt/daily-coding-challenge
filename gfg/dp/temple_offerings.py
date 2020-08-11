780d0948c088fa539deaedbef08716260e891af7
Rohit Rawat <rohitrawat2000@gmail.com>
2020-08-10 19:49:21 -0500

    temple offerings

---
+++ gfg/dp/temple_offerings.py
@@ -0,0 +1,40 @@
+"""
+Consider a devotee wishing to give offerings to temples along a mountain range.
+The temples are located in a row at different heights.
+Each temple should receive at least one offering.
+If two adjacent temples are at different altitudes,
+then the temple that is higher up should receive more offerings than the one that is lower down.
+If two adjacent temples are at the same height,
+then their offerings relative to each other does not matter.
+Given the number of temples and the heights of the temples in order,
+find the minimum number of offerings to bring.
+"""
+from typing import List
+
+
+def min_offerings(heights: List[int]) -> int:
+    """
+    Get the max increasing sequence on the left and the right side of current index,
+    leading upto the current index.
+    current index's value would be the max of both + 1.
+    """
+    length = len(heights)
+
+    if length < 2:
+        return length
+
+    left_inc = [0] * length
+    right_inc = [0] * length
+
+    for index in range(1, length):
+        if heights[index] > heights[index - 1]:
+            left_inc[index] = left_inc[index - 1] + 1
+        if heights[length - 1 - index] > heights[length - index]:
+            right_inc[length - 1 - index] = right_inc[length - index] + 1
+
+    return sum(1 + max(left_inc[index], right_inc[index]) for index in range(length))
+
+
+if __name__ == '__main__':
+    assert min_offerings([1, 2, 2]) == 4
+    assert min_offerings([1, 4, 3, 6, 2, 1]) == 10
