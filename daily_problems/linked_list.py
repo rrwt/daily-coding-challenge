45955373c1da646c42cd8b3d2afbde7766f298d9
Rohit Rawat <rohitrawat2000@gmail.com>
2020-09-02 05:44:53 -0500

    helper functions for linked list

--- daily_problems/linked_list.py
+++ daily_problems/linked_list.py
@@ -1,7 +1,39 @@
-from typing import Optional
+from typing import Optional, Union, List
 
 
 class Node:
     def __init__(self, data: int) -> None:
         self.data = data
         self.next: Optional[Node] = None
+
+
+def print_ll(head: Node) -> None:
+    runner = head
+
+    while runner.next:
+        print(runner.data, end="->")
+        runner = runner.next
+
+    if runner:
+        print(runner.data)
+
+
+def get_node_count(head: Node) -> int:
+    runner = head
+    count = 0
+
+    while runner:
+        count += 1
+        runner = runner.next
+
+    return count
+
+
+def create_ll_from_list(values: List[Union[int, str]]) -> Node:
+    head = tail = Node(values[0])
+
+    for element in values[1:]:
+        tail.next = Node(element)
+        tail = tail.next
+
+    return head
