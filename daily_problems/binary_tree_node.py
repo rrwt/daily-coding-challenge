5e5e7f1742d918d07fec32b4891610a9990c7590
Rohit Rawat <rohitrawat2000@gmail.com>
2020-09-29 14:28:40 -0500

    added level order traversal

--- daily_problems/binary_tree_node.py
+++ daily_problems/binary_tree_node.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Union, Optional
 
 
@@ -19,3 +20,25 @@ def inorder_traversal(root: Optional[Node]) -> None:
         inorder_traversal(root.left)
         print(root.data)
         inorder_traversal(root.right)
+
+
+def level_order_traversal(root: Node) -> None:
+    queue = deque()
+    queue.append((root, 0))
+    prev_level = 0
+
+    while queue:
+        node, level = queue.popleft()
+
+        if level != prev_level:
+            prev_level = level
+            print()
+
+        print(node.data, end=" ")
+
+        if node.left:
+            queue.append((node.left, level + 1))
+        if node.right:
+            queue.append((node.right, level + 1))
+
+    print()
